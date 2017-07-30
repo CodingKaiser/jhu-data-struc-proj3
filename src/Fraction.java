acecaf787c05d95a23443e4a5f3bdc2512d29660
CodingKaiser <falkonoe@live.com>
2017-07-30 16:08:03 -0700

    Added comments.

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,52 +1,116 @@
+/**
+ * A class representing a fraction. Consists of a numerator
+ * and denominator. Contains methods which provide add, divide,
+ * subtract, multiplication functionality on the current Fraction.
+ * @Author: Falko Noe
+ * @Version: 1.0
+ */
+
 public class Fraction {
 
-    int num;
-    int den;
+  private int num;
+  private int den;
 
-    public Fraction(int num, int den) {
-        this.num = num;
-        this.den = den;
-    }
+  /**
+   * Constructor for the Fraction. Returns an instance of
+   * Fraction with the specified denominator and numerator.
+   * @param num: The int representation of the numerator
+   * @param den: The int representation of the denominator
+   */
+  Fraction(int num, int den) {
+    this.num = num;
+    this.den = den;
+  }
 
-    public Fraction(int num) {
-        this.num = num;
-        this.den = 1;
-    }
+  /**
+   * Constructor for the Fraction.
+   * @param num: The int representation of the numerator
+   */
+  Fraction(int num) {
+    this.num = num;
+    this.den = 1;
+  }
 
-    public Fraction mult(Fraction x) {
-        return new Fraction(this.num * x.num, this.den * x.den);
-    }
+  /**
+   * Forms the product of two Fractions.
+   * @param x: The Fraction against which to multiply.
+   * @return: A new fraction that is the product of this fraction
+   * and the input. Does not return the reduced product.
+   */
+  Fraction mult(Fraction x) {
+    return new Fraction(this.num * x.num, this.den * x.den);
+  }
 
-    public Fraction div(Fraction x) {
-        if (x.num < 0) {
-            return new Fraction(this.num * x.den * -1, this.den * x.num * -1);
-        } else {
-            return new Fraction(this.num * x.den, this.den * x.num);
-        }
+  /**
+   * Forms the quotient of dividing the current Fraction by
+   * the input. Does not reduce.
+   * @param x: The Fraction against which to divide.
+   * @return: A new Fraction that is the division of the Fraction
+   * by the input.
+   */
+  Fraction div(Fraction x) {
+    if (x.num < 0) {
+      return new Fraction(this.num * x.den * -1, this.den * x.num * -1);
+    } else {
+      return new Fraction(this.num * x.den, this.den * x.num);
     }
+  }
 
-    public Fraction sub(Fraction x) {
-        return new Fraction(this.num * x.den - x.num * this.den, this.den * x.den);
-    }
+  /**
+   * Subtracts the current Fraction by the input and returns
+   * a new Fraction corresponding to the result.
+   * @param x: The input Fraction that will subtract from the input.
+   * @return: The Fraction result of subtracting the current
+   * Fraction by the input.
+   */
+  Fraction sub(Fraction x) {
+    return new Fraction(this.num * x.den - x.num * this.den,
+            this.den * x.den);
+  }
 
-    public Fraction add(Fraction x) {
-        return new Fraction(this.num * x.den + x.num * this.den, this.den * x.den);
-    }
+  /**
+   * Adds the current Fraction with the input and returns
+   * a Fraction that is the result of the operation.
+   * @param x: The input Fraction that will be added to the input.
+   * @return: The Fraction result of adding the current
+   * Fraction by the input.
+   */
+  Fraction add(Fraction x) {
+    return new Fraction(this.num * x.den + x.num * this.den,
+            this.den * x.den);
+  }
 
-    public boolean equalsZero() {
-        return this.num == 0;
-    }
+  /**
+   * A method which returns a boolean value based on
+   * whether the Fraction is equal to 0.
+   * @return: Returns true if the Fraction is equal to 0,
+   * false otherwise.
+   */
+  boolean equalsZero() {
+    return this.num == 0;
+  }
 
-    public int toInt() {
-        return this.num / this.den;
-    }
+  /**
+   * Converts the current Fraction to the result of dividing
+   * the numerator
+   * by the denominator.
+   * @return: The integer result of dividing the numerator
+   * by the denominator.
+   */
+  int toInt() {
+    return this.num / this.den;
+  }
 
-    @Override
-    public String toString() {
-        if (this.num == 0 || this.den == 1) {
-            return this.num + "";
-        } else {
-            return this.num + "/" + this.den;
-        }
+  /**
+   * Overrides the toString method of Object.
+   * @return: The string representation of the Fraction.
+   */
+  @Override
+  public String toString() {
+    if (this.num == 0 || this.den == 1) {
+      return this.num + "";
+    } else {
+      return this.num + "/" + this.den;
     }
+  }
 }
